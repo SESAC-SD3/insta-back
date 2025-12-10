2ab3878e66a6ba448f709835c9a9fb6061e97812
5chang2 <och8808@gmail.com>
2025-12-10 11:20:35 +0900

    Post read 기능 구현

--- src/main/java/com/example/instagramapi/controller/PostController.java
+++ src/main/java/com/example/instagramapi/controller/PostController.java
@@ -4,6 +4,7 @@ package com.example.instagramapi.controller;
 import com.example.instagramapi.dto.request.PostCreateRequest;
 import com.example.instagramapi.dto.response.ApiResponse;
 import com.example.instagramapi.dto.response.PostResponse;
+import com.example.instagramapi.entity.Post;
 import com.example.instagramapi.security.CustomUserDetails;
 import com.example.instagramapi.service.PostService;
 import jakarta.validation.Valid;
@@ -11,10 +12,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -31,4 +31,26 @@ public class PostController {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(ApiResponse.success(response));
     }
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<PostResponse>>> findAll() {
+        List<PostResponse> posts = postService.findAll();
+        return ResponseEntity.ok(ApiResponse.success(posts));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse<PostResponse>> findById(@PathVariable Long id) {
+        PostResponse response = postService.findById(id);
+        return ResponseEntity.ok(ApiResponse.success(response));
+    }
 }
+
+
+
+
+
+
+
+
+
+
